436a5a586d3fcebac98de8ff3bbf6f0b1c8a238d
Ten <Qoolebyaqa@gmail.com>
2023-02-05 02:22:16 +0500

    feat: add the hours breakdown

--- rs-clone/src/app/components/core/right-section/calendar/calendar.component.ts
+++ rs-clone/src/app/components/core/right-section/calendar/calendar.component.ts
@@ -9,8 +9,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 export class CalendarComponent {
   title = 'calendar';
-  @Output()
-  falser = new EventEmitter();
+  @Output() falser = new EventEmitter();
   hours = ['01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00',
   '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00', '24:00', ]
   now = (new Date).toDateString();
@@ -18,6 +17,13 @@ export class CalendarComponent {
   hidder(e: Event) {
     e.preventDefault();
     this.falser.emit(this.title);
+  }
+
+  Timepicker() {
+    if ((document.querySelector('.mat-datepicker-input') as HTMLInputElement).attributes[3].value == this.now) {
+      const curHour = (new Date).getHours();
+      return this.hours.filter((value: string) => Number(value[1]) > curHour);
+    } return this.hours;
   }
 
   nextDay () {
@@ -42,9 +48,10 @@ export class CalendarComponent {
     } else {
       dateInInput = new Date(dateInput.value)
     }
-
     let dateToPaste: Date = new Date();
     dateToPaste.setTime(dateInInput.getTime() - 86400000);
     dateInput.value = dateToPaste.toDateString();
   }
+
+
 }
