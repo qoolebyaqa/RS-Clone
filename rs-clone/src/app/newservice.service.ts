2d2f9a722f8cda3c412eac56969f5e43438a0ca6
Ten <Qoolebyaqa@gmail.com>
2023-02-15 20:37:50 +0500

    feat: start the emit interraction

--- rs-clone/src/app/newservice.service.ts
+++ rs-clone/src/app/newservice.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IlogUser, IregUser, ITask, IUser } from './classes/interfaces/interfaces';
+import { IlogUser, IregUser, ITask, IUser, UserPost } from './classes/interfaces/interfaces';
 import { BehaviorSubject, filter, Observable, Subject, Subscription } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
@@ -11,6 +11,11 @@ export class NewserviceService {
   username: string = '';
   tasks: ITask[] = [];
   users: IUser[] = [];
+
+  private tasks$: BehaviorSubject<any> = new BehaviorSubject([]);
+  tasksObs$: Observable<ITask[]> = this.tasks$.asObservable();
+  private users$: BehaviorSubject<any> = new BehaviorSubject([]);
+  usersObs$: Observable<IUser[]> = this.users$.asObservable();
   /* private tasks$: BehaviorSubject<any> = new BehaviorSubject([]);
   tasksObs$: Observable<ITask[]> = this.tasks$.asObservable();
   private users$: BehaviorSubject<any> = new BehaviorSubject([]);
@@ -45,14 +50,7 @@ export class NewserviceService {
   }
 
   public logUser(user: IlogUser): Observable<{email: string, name: string, token: string}> {
-    return this.http.post<{email: string, name: string, token: string}>('api/user/login', user)
-    /* .pipe(
-      tap(({token}) => {
-        localStorage.setItem('auth-token', token);
-        this.setToken(token);
-        console.log(user);
-      })
-    ); */
+    return this.http.post<{email: string, name: string, token: string}>('api/user/login', user);
   }
 
   public regUser(user: IregUser): Observable<{email: string, name: string, token: string}> {
@@ -78,7 +76,7 @@ export class NewserviceService {
   /* emitUser(data: string) {
     this.user$.next(data);
     this.user$.complete()
-  }
+  } */
   emitUsers(data: IUser[]) {
     this.users$.next(data);
     this.users$.complete();
@@ -86,7 +84,7 @@ export class NewserviceService {
   emitTasks(data: ITask[]) {
     this.tasks$.next(data);
     this.tasks$.complete();
-  } */
+  }
 }
 
 
