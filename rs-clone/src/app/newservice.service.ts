690a0a0c3496232f761bdd090a8d2a79b344858f
Ten <Qoolebyaqa@gmail.com>
2023-02-17 21:49:01 +0500

    feat: add the error interceptor

--- rs-clone/src/app/newservice.service.ts
+++ rs-clone/src/app/newservice.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IlogUser, IregUser, ITask, IUser, UserPost } from './classes/interfaces/interfaces';
-import { BehaviorSubject, filter, Observable, Subject, Subscription } from 'rxjs';
+import { BehaviorSubject, delay, filter, mergeMap, Observable, of, retryWhen, Subject, Subscription, throwError } from 'rxjs';
 
+function retryPls <T> (delayMS = 1000, tries = 5) {
+  let retries = tries;
+  let arrErrors: any[] = [];
+  return (src: Observable<T>): Observable<T> => src.pipe(
+    retryWhen(err => err.pipe(
+      delay(delayMS),
+      mergeMap(err => {
+        arrErrors.push(err);
+        return --retries > 0 ? of(err) : throwError({ err: 'Mnogo' });
+      })
+    ))
+  )
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -40,32 +53,39 @@ export class NewserviceService {
   }
 
   public logUser(user: IlogUser): Observable<{email: string, name: string, token: string}> {
-    return this.http.post<{email: string, name: string, token: string}>('api/user/login', user);
+    return this.http.post<{email: string, name: string, token: string}>('api/user/login', user)
+    .pipe(retryPls());
   }
 
   public regUser(user: IregUser): Observable<{email: string, name: string, token: string}> {
-      return this.http.post<{email: string, name: string, token: string}>('api/user/register', user);
+      return this.http.post<{email: string, name: string, token: string}>('api/user/register', user)
+      .pipe(retryPls());
     }
 
   public deleteData(id: string): Observable<any> {
-    return this.http.delete<any>(`api/workouts/${id}`);
+    return this.http.delete<any>(`api/workouts/${id}`)
+    .pipe(retryPls());
   }
 
   public updateData(id: string, task: {}): Observable<any> {
-    return this.http.patch<any>(`api/workouts/${id}`, task);
+    return this.http.patch<any>(`api/workouts/${id}`, task)
+    .pipe(retryPls());
   }
 
 
   public getData(): Observable<any> {
-    return this.http.get('api/workouts');
+    return this.http.get('api/workouts')
+    .pipe(retryPls());
   }
 
   public setData(task: ITask): Observable<any> {
-    return this.http.post('api/workouts', task);
+    return this.http.post('api/workouts', task)
+    .pipe(retryPls());
   }
 
   public getUsers(): Observable<any> {
-    return this.http.get('api/user/users');
+    return this.http.get('api/user/users')
+    .pipe(retryPls());
   }
   emitUsers(data: IUser[]) {
     this.users$.next(data);
